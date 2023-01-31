86b11ce86fbec0973216529711f725f017d9a117
cetinkayayusuf <39012641+cetinkayayusuf@users.noreply.github.com>
2023-01-31 11:07:10 +0300

    Add DeleteBookCommand

--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -4,6 +4,7 @@ using WebApi.BookOperations.GetBooks;
 using WebApi.BookOperations.AddBook;
 using WebApi.BookOperations.UpdateBook;
 using WebApi.BookOperations.GetBookDetail;
+using WebApi.BookOperations.DeleteBook;
 
 namespace WebApi.Controllers
 {
@@ -77,11 +78,16 @@ namespace WebApi.Controllers
         [HttpDelete("{id}")]
         public IActionResult DeleteBook(int id)
         {
-            var book = _context.Books.SingleOrDefault(x => x.Id == id);
-            if (book is null)
-                return BadRequest();
-            _context.Books.Remove(book);
-            _context.SaveChanges();
+            DeleteBookCommand command = new DeleteBookCommand(_context);
+            try
+            {
+                command.BookId = id;
+                command.Handle();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
             return Ok();
         }
     }
