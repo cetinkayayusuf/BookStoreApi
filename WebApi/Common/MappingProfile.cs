3e7da3e37c5164e3a6747c5149fdc8c63894be94
cetinkayayusuf <39012641+cetinkayayusuf@users.noreply.github.com>
2023-02-01 18:08:13 +0300

    Update Mapping Profile

--- WebApi/Common/MappingProfile.cs
+++ WebApi/Common/MappingProfile.cs
@@ -1,4 +1,8 @@
 using AutoMapper;
+using WebApi.Application.AuthorOperations.Commands.AddAuthor;
+using WebApi.Application.AuthorOperations.Commands.UpdateAuthor;
+using WebApi.Application.AuthorOperations.Queries.GetAuthorDetail;
+using WebApi.Application.AuthorOperations.Queries.GetAuthors;
 using WebApi.Application.BookOperations.Commands.AddBook;
 using WebApi.Application.BookOperations.Queries.GetBookDetail;
 using WebApi.Application.BookOperations.Queries.GetBooks;
@@ -17,11 +21,13 @@ namespace WebApi.Common
 
             CreateMap<Book, BookDetailViewModel>()
                 .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name))
-                .ForMember(dest => dest.PublishDate, opt => opt.MapFrom(src => src.PublishDate.Date.ToString("dd/MM/yyyy")));
+                .ForMember(dest => dest.PublishDate, opt => opt.MapFrom(src => src.PublishDate.Date.ToString("dd/MM/yyyy")))
+                .ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.Author.FirstName + " " + src.Author.LastName));
 
             CreateMap<Book, BooksViewModel>()
                 .ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name))
-                .ForMember(dest => dest.PublishDate, opt => opt.MapFrom(src => src.PublishDate.Date.ToString("dd/MM/yyyy")));
+                .ForMember(dest => dest.PublishDate, opt => opt.MapFrom(src => src.PublishDate.Date.ToString("dd/MM/yyyy")))
+                .ForMember(dest => dest.Author, opt => opt.MapFrom(src => src.Author.FirstName + " " + src.Author.LastName));
 
             CreateMap<AddGenreModel, Genre>();
 
@@ -30,6 +36,18 @@ namespace WebApi.Common
 
             CreateMap<Genre, GenresViewModel>()
                 .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name));
+
+            CreateMap<AddAuthorModel, Author>();
+
+            CreateMap<UpdateAuthorModel, Author>();
+
+            CreateMap<Author, AuthorDetailViewModel>()
+                .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => src.BirthDate.Date.ToString("dd/MM/yyyy")))
+                .ForMember(dest => dest.Books, opt => opt.MapFrom(src => src.Book.Select(x => x.Title)));
+
+            CreateMap<Author, AuthorsViewModel>()
+                .ForMember(dest => dest.BirthDate, opt => opt.MapFrom(src => src.BirthDate.Date.ToString("dd/MM/yyyy")))
+                .ForMember(dest => dest.Books, opt => opt.MapFrom(src => src.Book.Select(x => x.Title)));
         }
     }
 }
