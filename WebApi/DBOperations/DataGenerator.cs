b64ebd7abdb01c9d339b725a67d752b48c8b6c25
cetinkayayusuf <39012641+cetinkayayusuf@users.noreply.github.com>
2023-02-01 18:08:54 +0300

    Update DBContext and DataGeneration

--- WebApi/DBOperations/DataGenerator.cs
+++ WebApi/DBOperations/DataGenerator.cs
@@ -12,6 +12,20 @@ namespace WebApi.DBOperations
                 if (context.Books.Any())
                     return;
 
+                context.Authors.AddRange(
+                                    new Author
+                                    {
+                                        FirstName = "Cem1",
+                                        LastName = "Cem2",
+                                        BirthDate = new DateTime(2005, 1, 3)
+                                    },
+                                    new Author
+                                    {
+                                        FirstName = "Cam1",
+                                        LastName = "Cam2",
+                                        BirthDate = new DateTime(2001, 1, 1)
+                                    }
+                                );
                 context.Genres.AddRange(
                     new Genre
                     {
@@ -29,14 +43,24 @@ namespace WebApi.DBOperations
                         Title = "Start With Why",
                         GenreId = 1,
                         PageCount = 274,
-                        PublishDate = new DateTime(2005, 1, 3)
+                        PublishDate = new DateTime(2005, 1, 3),
+                        AuthorId = 1
                     },
                     new Book
                     {
                         Title = "Start With Dinner",
                         GenreId = 2,
                         PageCount = 172,
-                        PublishDate = new DateTime(2004, 2, 2)
+                        PublishDate = new DateTime(2004, 2, 2),
+                        AuthorId = 2
+                    },
+                    new Book
+                    {
+                        Title = "Start With Breakfast",
+                        GenreId = 1,
+                        PageCount = 1232,
+                        PublishDate = new DateTime(2003, 2, 2),
+                        AuthorId = 2
                     }
                 );
                 context.SaveChanges();
