5d8daf2cc1dd4d1aaa7f3047b358480f16bc091e
cetinkayayusuf <39012641+cetinkayayusuf@users.noreply.github.com>
2023-02-05 21:09:04 +0300

    Add Control of being not null to do trim

--- WebApi/Application/AuthorOperations/Commands/UpdateAuthor/UpdateAuthorCommand.cs
+++ WebApi/Application/AuthorOperations/Commands/UpdateAuthor/UpdateAuthorCommand.cs
@@ -1,5 +1,4 @@
 using WebApi.DBOperations;
-using AutoMapper;
 
 namespace WebApi.Application.AuthorOperations.Commands.UpdateAuthor
 {
@@ -19,9 +18,9 @@ namespace WebApi.Application.AuthorOperations.Commands.UpdateAuthor
             if (author is null)
                 throw new InvalidOperationException("Author not found");
 
-            author.FirstName = string.IsNullOrEmpty(Model.FirstName.Trim()) ? author.FirstName : Model.FirstName;
-            author.LastName = string.IsNullOrEmpty(Model.LastName.Trim()) ? author.LastName : Model.LastName;
-            author.BirthDate = Model.BirthDate != default ? Model.BirthDate : author.BirthDate;
+            author.FirstName = string.IsNullOrEmpty(Model.FirstName?.Trim()) ? author.FirstName : Model.FirstName;
+            author.LastName = string.IsNullOrEmpty(Model.LastName?.Trim()) ? author.LastName : Model.LastName;
+            author.BirthDate = Model.BirthDate == default ? author.BirthDate : Model.BirthDate;
 
             _dbContext.SaveChanges();
         }
